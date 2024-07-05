f1c01088d9d3e939cde6d4ee1c62c8110c04ea9e
Alexander Sedam <alexander.sedam@gmail.com>
2024-07-05 12:27:10 -0400

    commit

---
+++ MicrosoftPython/SpaceProgram.py
@@ -0,0 +1,10 @@
+#from datetime import date
+#date = date.today()
+#print(date)
+##############################
+# UNIT CONVERSION
+
+parsecs = 11
+lightyears = parsecs * 3.26
+
+print(str(parsecs) + "parsecs is " + str(lightyears) + " lightyears")
